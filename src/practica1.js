a1cddc52a92bf63623d731d88b8f975cf8bdfa80
MrRleft <Rubenizq@ucm.es>
2018-02-22 15:40:04 +0100

    Initial commit

---
+++ src/practica1.js
@@ -0,0 +1,70 @@
+/**
+ * MemoryGame es la clase que representa nuestro juego. Contiene un array con la cartas del juego,
+ * el número de cartas encontradas (para saber cuándo hemos terminado el juego) y un texto con el mensaje
+ * que indica en qué estado se encuentra el juego
+ */
+var MemoryGame = MemoryGame || {};
+
+/**
+ * Constructora de MemoryGame
+ */
+MemoryGame = function(gs) {
+
+	this.InitGame = function(){
+
+	}
+
+	this.draw = function(){
+
+	}
+
+	this.loop = function(){
+
+		var that = this;
+		setInterval(function(){
+			that.draw()}
+			, 16);
+	}
+
+	this.onClick = function(){
+
+
+	}
+
+};
+
+
+
+/**
+ * Constructora de las cartas del juego. Recibe como parámetro el nombre del sprite que representa la carta.
+ * Dos cartas serán iguales si tienen el mismo sprite.
+ * La carta puede guardar la posición que ocupa dentro del tablero para luego poder dibujarse
+ * @param {string} id Nombre del sprite que representa la carta
+ */
+MemoryGameCard = function(id) {
+
+	this.MemoryGameCard = function(sprite){
+
+
+	}
+
+	this.flip = function(){
+
+
+	}
+
+	this.found = function(){
+
+
+	}
+
+	this.compareTo = function(otherCard){
+
+
+	}
+
+	this.draw = function(gs, pos){
+
+		
+	}
+};
